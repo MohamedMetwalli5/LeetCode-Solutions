8e9f90f7a576d7164f358168d093a8602f387694
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-04-04 13:55:40 +0200

    Update 1614.java

--- Solutions/1614.java
+++ Solutions/1614.java
@@ -1,19 +1,13 @@
 class Solution {
     public int maxDepth(String s) {
-        Stack<Character> st = new Stack<>();
-        int max = Integer.MIN_VALUE, counter = 0;
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
-                st.push('(');
-                counter++;
-            }else if(s.charAt(i) == ')'){
-                st.pop();
-                counter--;
+         int count = 0, max = 0;  
+        for (int i=0;i<s.length();i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            }else if(s.charAt(i) == ')') {
+                count--;
             }
-            if(counter > max){
-                max = counter;
-            }
+            max = Math.max(count, max);
         }
         return max;
     }
