5d3722a3ff6ed88dbeec491fc335cf7a54da465a
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-06 19:27:16 +0300

    Update 1109.java

--- Solutions/1109.java
+++ Solutions/1109.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int[] arr = new int[n];
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         for(int i=0;i<bookings.length;i++){
-            for(int j=bookings[i][0]-1;j<bookings[i][1];j++){
-                arr[j] += bookings[i][2];
+            int first = bookings[i][0];
+            int last = bookings[i][1];
+            for (int j=first;j<=last;j++) {
+                arr[j-1] += bookings[i][2];
             }
         }
         return arr;
