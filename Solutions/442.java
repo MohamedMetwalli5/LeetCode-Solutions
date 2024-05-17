4a20637db9d839077cfb90b6cd1a48690e48c59a
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-17 20:34:16 +0300

    Update 442.java

--- Solutions/442.java
+++ Solutions/442.java
@@ -1,14 +1,24 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        HashSet<Integer> h = new HashSet<>();
-        List<Integer> l = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(h.contains(nums[i])){
-                l.add(nums[i]);                
+        List<Integer> l =  new ArrayList<>();
+        int i=0;
+        while(i < nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] =  temp;
             }else{
-                h.add(nums[i]);
+                i++;
             }
         }
+
+        for(int index=0;index < nums.length;index++){
+            if(nums[index] != index+1){
+                l.add(nums[index]);
+            }
+        }
+
         return l;
     }
 }
