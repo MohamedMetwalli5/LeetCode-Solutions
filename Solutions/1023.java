3c036a8dff885407e293a9f9165e95e0b624fe9d
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-17 19:47:25 +0300

    Update 1023.java

--- Solutions/1023.java
+++ Solutions/1023.java
@@ -1,57 +1,29 @@
 class Solution {
-    
-    class Trie {
-        TrieNode root;
-        List<Character> CapitalLetters;
-        class TrieNode {
-            Map<Character, TrieNode> nodes = new HashMap<>();
-        }
-
-        public Trie() { 
-            root = new TrieNode(); 
-            CapitalLetters = new ArrayList<>();
-        }
-
-        public void insert(String pattern) {
-            TrieNode current = root;
-            char[] PatternCharArray = pattern.toCharArray();
-            for(Character c : PatternCharArray) {
-                if(Character.isUpperCase(c)){ 
-                    CapitalLetters.add(c);
-                }
-                if(!current.nodes.containsKey(c)){
-                    current.nodes.put(c, new TrieNode());
-                }
-                current = current.nodes.get(c);            
-            }
-        }
-
-        public boolean search(String query) {
-            TrieNode current = root;
-            List<Character> CurrentCharactersList = new ArrayList<>(CapitalLetters);
-            char[] QueryCharArray = query.toCharArray();
-            for(Character c : QueryCharArray) {
-                if(!current.nodes.containsKey(c)){
-                    if(Character.isUpperCase(c)){ 
-                        return false;
-                    }else{
-                        continue;
-                    }
-                }
-                CurrentCharactersList.remove(c);
-                current = current.nodes.get(c);
-            }
-            return CurrentCharactersList.size() == 0;
-        }
-    }
-
     public List<Boolean> camelMatch(String[] queries, String pattern) {
-        Trie trie = new Trie();
-        trie.insert(pattern);
-        List<Boolean> l = new ArrayList<>();
-        for(int i=0;i<queries.length;i++){ 
-            l.add(trie.search(queries[i]));
+        List<Boolean> result = new ArrayList<>();
+        for(int i=0;i<queries.length;i++) {
+           result.add(checkIfFollow(queries[i],pattern));
         }
-        return l;
+        return result;       
     }
+
+    public boolean checkIfFollow(String query,String pattern){
+        int i=0, j=0;
+        while (i < query.length() && j < pattern.length()) {
+            if (query.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+            }else {
+                if (Character.isUpperCase(query.charAt(i))) {
+                    return false;
+                    
+                }
+                i++;
+            }
+        }
+
+        return j == pattern.length() && query.substring(i).chars().allMatch(Character::isLowerCase);
+    }
+    
+           
 }
