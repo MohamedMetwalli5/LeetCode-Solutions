84d9443af2c8dce0f009f67aba4bbce6f92a13b1
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-17 10:30:20 +0300

    Update 1769.java

--- Solutions/1769.java
+++ Solutions/1769.java
@@ -1,17 +1,27 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int[] res = new int[boxes.length()];
-        for(int i=0;i<res.length;i++){
-            for(int j=i+1;j<boxes.length();j++){
-                if(boxes.charAt(j) == '1'){
-                    res[i] += Math.abs(i-j);
-                }
+        int n = boxes.length();
+        int[] res = new int[n];
+        
+        //calculate the operations needed from left to right
+        int count=0, sum=0;
+        for(int i=1;i<n;i++) {
+            if(boxes.charAt(i - 1) == '1'){
+                count++;
             }
-            for(int j=i-1;j>=0;j--){
-                if(boxes.charAt(j) == '1'){
-                    res[i] += Math.abs(i-j);
-                }
+            sum += count;
+            res[i] = sum;
+        }
+        
+        //calculate the operations needed from right to left
+        count = 0; 
+        sum = 0;
+        for(int i=n-2;i>=0;i--) {
+            if(boxes.charAt(i + 1) == '1'){
+                count++;
             }
+            sum += count;
+            res[i] += sum;
         }
         
         return res;
