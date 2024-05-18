a4994e4f3db6d22ef86f97aeff07042d42432915
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-18 22:27:20 +0300

    Update 347.java

--- Solutions/347.java
+++ Solutions/347.java
@@ -1,34 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer> l = new ArrayList<>();
         HashMap<Integer, Integer> h = new HashMap<>();
+        PriorityQueue<Pair<Integer,Integer>> minPQ = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         for(int i=0;i<nums.length;i++){
-            if(h.get(nums[i]) == null){
-                h.put(nums[i], 1);
-            }else{
-                h.put(nums[i], h.get(nums[i])+1);
+            int freq = h.getOrDefault(nums[i], 0) + 1;
+            h.put(nums[i], freq);
+        }
+        for(HashMap.Entry<Integer, Integer> e : h.entrySet()){
+            minPQ.add(new Pair(e.getKey(), e.getValue()));
+            if(minPQ.size() > k){
+                minPQ.remove();
             }
         }
-        int[] temp = new int[h.size()];
-        int z = 0;
-        for(Integer i : h.keySet()){
-            temp[z++] = h.get(i);
-        }
-        Arrays.sort(temp);
         int[] res = new int[k];
-        z = 0;
-        for(Integer i : h.keySet()){
-            if(z < res.length){
-                for(int j=temp.length-1;j>=temp.length-k;j--){
-                    if(h.get(i) == temp[j]){
-                        res[z++] = i;
-                        break;
-                    }
-                }
-            }else{
-                break;
-            }
+        for(int i=0; i < k;i++){
+            res[i] = minPQ.remove().getKey();
         }
+        
         return res;
     }
 }
