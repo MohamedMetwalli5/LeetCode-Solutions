caffa50d62548c18193681ebd079d5f8a3da64c5
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-18 13:35:53 +0300

    Update 2125.java

--- Solutions/2125.java
+++ Solutions/2125.java
@@ -1,23 +1,20 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int res = 0;
-        List<Integer> l = new ArrayList<>();
-        for(int i=0;i<bank.length;i++){
-            int sum = 0;
-            for(int j=0;j<bank[i].length();j++){
-                if(bank[i].charAt(j) == '1'){
-                    sum++;
+        int res=0, prev=0;
+        for(int i=0;i < bank.length;i++){
+            int count=0;
+            String s = bank[i];
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(j) == '1'){
+                    count++;
                 }
             }
-            if(sum > 0){
-                l.add(sum);
+            if(count != 0){
+                res += prev * count;
+                prev = count;
             }
         }
-        
-        for(int i=0;i<l.size()-1;i++){
-            res += (l.get(i)*l.get(i+1));
-        }
-        
+
         return res;
     }
 }
