f4389e59bbdb54d5a5c9026dfbb1143e4872dfc0
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2024-05-18 15:39:15 +0300

    Update 973.java

--- Solutions/973.java
+++ Solutions/973.java
@@ -1,34 +1,19 @@
+import java.util.PriorityQueue;
+
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] res = new int[k][points[0].length];
-        HashMap<Double, Set<int[][]>> h = new HashMap<>();
-        double[] distance = new double[points.length];
-        for(int i=0;i<points.length;i++){
-            distance[i] = Math.sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]);
-            if (!h.containsKey(distance[i])) {
-                h.put(distance[i], new HashSet<>());
-            }
-            h.get(distance[i]).add(new int[][]{{points[i][0], points[i][1]}});
-            
-        }
-        
-        Arrays.sort(distance);
-        
-        int w = 0;
-        for(int i=0;i<k;i++){
-            int flag = 0;
-            for(int[][] s : h.get(distance[i])){
-                res[w][0] = s[0][0];
-                res[w++][1] = s[0][1];
-                // System.out.println(s[0][0] + ", " + s[0][1]);
-                if(flag == 1){
-                    i++;   
-                }else{
-                    flag = 1;
-                }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0]*b[0] + b[1]*b[1], a[0]*a[0] + a[1]*a[1]));
+        for(int[] point : points){
+            pq.offer(point);
+            if(pq.size() > k) {
+                pq.poll();
             }
         }
-        
+        int[][] res = new int[k][2];
+        for(int i=0; i < k;i++){
+            res[i] = pq.poll();
+        }
+
         return res;
     }
 }
